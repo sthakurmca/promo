b9b349a99be6cb1d9072f371cab12551e462e81c
sachin <sthakur.mca@gmail.comn>
2022-09-18 20:35:31 +0530

    first commit

--- src/component/Main.js
+++ src/component/Main.js
@@ -14,7 +14,6 @@ export default function Main() {
                     return a.sequence - b.sequence;
                 });
                 localStorage.setItem("data", JSON.stringify(data));
-                //setList(data);
                 setList(data);
             }).catch((err) => {
                 setList([]);
